98a4e83c20a136ccef6afd561c0c9bdb8844313d
Haider Imtiaz <haider.imtiaz38@ee.ceme.edu.pk>
2024-09-24 01:03:47 +0500

    My CV

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/left-sidebar')
+def left_sidebar():
+    return render_template('left-sidebar.html')
+
+@app.route('/right-sidebar')
+def right_sidebar():
+    return render_template('right-sidebar.html')
+
+@app.route('/no-sidebar')
+def no_sidebar():
+    return render_template('no-sidebar.html')
+
+@app.route('/elements')
+def elements():
+    return render_template('elements.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
